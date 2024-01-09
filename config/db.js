abb0cb976caf95c9289f23f44abad72cdef95efc
u-minh <dev.minhquang@gmail.com>
2024-01-09 21:46:58 +0700

    Fix connect transaction to MongoDB

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
+    // Get connection string to connect to MongoDB
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Conneted To MongoDB Databse ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Error in MongoDB ${error}`.bgRed.white);
   }
 };
 
