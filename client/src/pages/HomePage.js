17fd2c3ae673a2f94ddedfbfef4397ea16f28bcd
Dat <101729787+phubanh0208@users.noreply.github.com>
2024-05-13 20:40:18 +0700

    dđ

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -340,56 +340,61 @@ const HomePage = () => {
         <div className="col-md-10">
           <h1 className="text-center">All Products</h1>
           <div className="d-flex flex-wrap">
-            {products && products.length > 0 ? (
-              products.map((p) => (
-                <div className="card m-3" key={p._id}>
-
-                  <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
-                    className="card-img-top"
-                    alt={p.name}
-                  />
-                  <div className="card-body">
-                    <div className="card-name-price">
-                      <h5 className="card-title">{p.name}</h5>
-                      <h5 className="card-title card-price">
-                        {p.price.toLocaleString("en-US", {
-                          style: "currency",
-                          currency: "VND",
-                        })}
-                      </h5>
-                    </div>
-                    <p className="card-text ">
-                      {p.description.substring(0, 60)}...
-                    </p>
-                    <div className="card-name-price">
-                      <button
-                        className="btn btn-info ms-1"
-                        onClick={() => navigate(`/product/${p.slug}`)}
-                      >
-                        More Details
-                      </button>
-                      <button
-                        className="btn btn-dark ms-1"
-                        onClick={() => {
-                          setCart([...cart, p]);
-                          localStorage.setItem(
-                            "cart",
-                            JSON.stringify([...cart, p])
-                          );
-                          toast.success("Item Added to cart");
-                        }}
-                      >
-                        ADD TO CART
-                      </button>
-                    </div>
-                  </div>
-
-                </div>
-              ))
-            ) : (
-              <p className="text-center">No products found.</p>
-            )}
+          {products && products.length > 0 ? (
+  products.map((p) => (
+    <div className={`card m-3 ${p.quantity === 0 ? 'out-of-stock' : ''}`} key={p._id}>
+      <img
+        src={`/api/v1/product/product-photo/${p._id}`}
+        className="card-img-top"
+        alt={p.name}
+      />
+      <div className="card-body">
+        <div className="card-name-price">
+          <h5 className="card-title">{p.name}</h5>
+          <h5 className="card-title card-price">
+            {p.price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "VND",
+            })}
+          </h5>
+        </div>
+        <p className="card-text ">
+          {p.description.substring(0, 60)}...
+        </p>
+        <div className="card-name-price">
+          <button
+            className="btn btn-info ms-1"
+            onClick={() => navigate(`/product/${p.slug}`)}
+          >
+            More Details
+          </button>
+          {p.quantity > 0 ? (
+            <button
+              className="btn btn-dark ms-1"
+              onClick={() => {
+                setCart([...cart, p]);
+                localStorage.setItem(
+                  "cart",
+                  JSON.stringify([...cart, p])
+                );
+                toast.success("Item Added to cart");
+              }}
+            >
+              ADD TO CART
+            </button>
+          ) : (
+            <button className="btn btn-dark ms-1" disabled>
+              OUT OF STOCK
+            </button>
+          )}
+        </div>
+      </div>
+   
+    </div>
+  ))
+) : (
+  <p className="text-center">No products found.</p>
+)}
           </div>
           {products && products.length < total && products.length > 0 && (
             <div className="m-2 p-3">
